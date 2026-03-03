3cd79e89dc644c847d26151e11c6b1c3a5eef476
4anghyeon <uphyeon@kakao.com>
2026-03-03 09:57:23 +0900

    fix: remove tracked .gitignore from inlang project

---
+++ packages/i18n/src/index.ts
@@ -0,0 +1,7 @@
+export * as m from "./paraglide/messages.js";
+export * from "./paraglide/runtime.js";
+
+export {
+  getLocale as languageTag,
+  setLocale as setLanguageTag,
+} from "./paraglide/runtime.js";
