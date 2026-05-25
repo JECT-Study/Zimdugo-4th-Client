4bbad4e3ac0e38a9adc0864f4694a95aac4f97ba
jong-bin baik <164011256+Binz-collab@users.noreply.github.com>
2026-05-25 15:13:13 +0900

    fix: 홈 화면 네이버 지도 렌더링 연결

--- apps/web/src/entities/map/index.ts
+++ apps/web/src/entities/map/index.ts
@@ -2,3 +2,4 @@ export * from "./model/NaverMapProvider";
 export * from "./model/current-location";
 export * from "./model/naver-map-language";
 export * from "./model/useCurrentLocationFocus";
+export * from "./ui/NaverMapCanvas";
